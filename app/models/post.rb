795eb8f2e00cbd3265b8a8448af10da6c5c0664c
Ian Cossentino <icossentino@gmail.com>
2021-01-16 16:29:18 -0500

    commit tree 9d618a7d18203b090b89a3f5ef96534316628a6a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :must_be_clickbait
+
+    def must_be_clickbait
+        if self.title
+            title = self.title
+            clickbait_title_words = ["Guess", "Secret", "Won't Believe"]
+            if (clickbait_title_words.none? { |word| title.include?(word) }) && (!/Top\s\d+/.match?(title))
+                errors.add(:title, "must be clickbait")
+            end
+        end
+    end
+
+
+          #  if title includes any of the following, raise and exception
+
 end
